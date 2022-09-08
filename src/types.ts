c436c8943313a74b4ad4fe5757f25be4cf8d2440
apollo79 <bummtschack@mail.de>
2022-09-08 19:26:28 +0200

    feat(types): add necessary types for messenger
    added types:
    - Status: a users online status
    - UserId
    - User
    - MessageId
    - Message
    - DmMessage  (maybe to be removed)
    - ChatId
    - ChatType (maybe to be removed)
    - Chat

--- src/types.ts
+++ src/types.ts
@@ -4,4 +4,55 @@ type PropsWithChildren = { children: JSXElement };
 
 type ExtendPropsWithChildren<T extends Record<string, unknown> = Record<never, never>> = T & PropsWithChildren;
 
-export type { ExtendPropsWithChildren, PropsWithChildren };
+type Status = 'online' | 'offline' | 'working';
+
+type UserId = string;
+
+type User = {
+    id: UserId;
+    name: string;
+    status: Status;
+};
+
+type MessageId = number;
+
+type Message = {
+    id: MessageId;
+    content: string;
+    createdAt: Date;
+    edited: boolean;
+    user: UserId;
+};
+
+type DmMessage = Message & {
+    read: boolean;
+};
+
+type ChatId = number;
+
+type ChatType = 'dm' | 'group';
+
+type Chat = {
+    id: ChatId;
+    type: ChatType;
+    messages: Message[] | DmMessage[];
+    countUnread: number;
+    name?: string;
+    users?: UserId[];
+    user?: UserId;
+};
+
+export type {
+    ExtendPropsWithChildren,
+    PropsWithChildren,
+    Status,
+    Epoch,
+    UserId,
+    User,
+    MessageId,
+    Message,
+    DmMessage,
+    ChatId,
+    ChatType,
+    Chat,
+};

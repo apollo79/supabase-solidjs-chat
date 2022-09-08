9153270788ebdff2c4354c2ee1149208411c9930
apollo79 <bummtschack@mail.de>
2022-09-08 19:06:52 +0200

    fix: add typings to supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '~/database.types';
 
 const supabaseUrl = String(import.meta.env.VITE_SUPABASE_URL);
 const supabaseAnonKey = String(import.meta.env.VITE_SUPABASE_ANON_KEY);
 
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
 
 export { supabaseClient as supabase };
 
